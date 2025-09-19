95a495c7281f8d0731d33653e40b04156edbd124
kimjunghwannn <aa2002dcks@naver.com>
2025-09-19 13:29:08 +0900

    Initial commit

---
+++ src/main/java/com/example/koaskproject/KoasKprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.koaskproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoasKprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoasKprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/koaskproject/KoasKprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koaskproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoasKprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
